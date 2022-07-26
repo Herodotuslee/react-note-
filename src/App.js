88bc0046828534cc3907fdd841f6381c919dee06
Albert Lee <herodotuslee@gmail.com>
2022-07-25 21:18:17 -0700

    map() call back

--- src/App.js
+++ src/App.js
@@ -3,37 +3,43 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-       //?
-    this.state ={
-      name:{firstName:"Albert000",lastName:"Chen"}
+    //?
+    this.state = {
+      name: { firstName: "Albert000", lastName: "Chen" },
+      monsters: [{ name: 'Aaa' }, { name: 'BBB' }, { name: "CCC" }]
     }
 
- 
+
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-         <p>Hi {this.state.name.firstName+"  "+this.state.name.lastName}</p>
-         <butto onClick={
-          ()=>{
-            this.setState(
-              ()=>{
-                return  {name:{firstName:'Albert111',lastName:'LEE'}
-              };
-            },
-            ()=>{
-              console.log('this.state',this.state)
+          <p>Hi {this.state.name.firstName + "  " + this.state.name.lastName}</p>
+          <button onClick={
+            () => {
+              this.setState(
+                // async
+                () => {
+                  return {
+                    name: { firstName: 'Albert111', lastName: 'LEE' }
+                  };
+                },
+                () => {
+                  console.log('this.state', this.state)
+                }
+                //call back
+              )
             }
-            //call back
-            )
-          }
-        
-         }>Change Name</butto>
+
+          }>Change Name</button>
+          {this.state.monsters.map((monster)=>{return <h1>{monster.name}</h1>})}
+          {/* put call back function in map() */}
         </header>
+
       </div>);
   }
 }
