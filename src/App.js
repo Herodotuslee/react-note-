a86e36a085d417ea5d9687a09db2627b6faf2279
Albert Lee <herodotuslee@gmail.com>
2022-07-26 21:38:25 -0700

    move fn out of render to improve performance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import { render } from '@testing-library/react';
 
 class App extends Component {
+
+  // constructor-> render->componentDidMount
+
+  // initialize the state
   constructor() {
     super();
     //?
     this.state = {
       name: { firstName: "Albert000", lastName: "Chen" },
+      searchField:"",
       monsters: [
         {
           name: 'Aaa',
@@ -22,25 +28,57 @@ class App extends Component {
           id: 'fsdfsfs'
         }]
     }
+    console.log('1111')
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response=>response.json())
-    .then((users)=>
-    this.setState(
-      ()=>{
-        return {monsters:users}
-      },
-      ()=>{
-        console.log('this.state',this.state)
-      }))
+
+  // improve performance
+  onSearchChange = (event) => {
+    console.log('event', event)
+    const searchString =event.target.value.toLowerCase();
+    console.log('filterMonster',searchString)
+    // const filterMonster = this.state.monsters.filter((monster) => {
+    //   return monster.name.toLowerCase().includes(searchString);
+  
+    // })
+    // console.log('filterMonster',filterMonster)
+    this.setState(() => {
+      // return { monsters: filterMonster }
+      return {searchField:searchString}
+    })
   }
+
+  componentDidMount() {
+    console.log('33333')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log('this.state', this.state)
+          }))
+  }
+  // after componentDidMount->re render()
   render() {
+
+    const {monsters,searchField}=this.state;
+    const {onSearchChange}=this
+    // improve performance
+    console.log('2222')
+    const filteredMonsters =monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+  
+    })
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monster'
+
+          onChange={ onSearchChange}></input>
+        {/* eventhandler */}
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>Hi {this.state.name.firstName + "  " + this.state.name.lastName}</p>
           <button onClick={
             () => {
@@ -59,7 +97,9 @@ class App extends Component {
             }
 
           }>Change Name</button>
-          {this.state.monsters.map((monster) => {
+
+          
+          {filteredMonsters.map((monster) => {
             return (
               <div key={monster.id}
               ><h1 >{monster.name}</h1>
